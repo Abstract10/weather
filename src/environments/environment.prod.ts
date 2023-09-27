3b45f9d61f2bea6753a5c21d4c5a0bd312a2e0a2
Abhijeet <abhijeet@Abhijeet.Abhijeet-Kali>
2023-09-27 11:39:20 +0530

    final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
     XRapidAPIHostHeaderName:'X-RapidAPI-Host',
     XRapidAPIHostHeaderValue:'openweather43.p.rapidapi.com',
     XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
-    XRapidAPIKeyHeaderValue:'cb1c70ae9emsh3f5025072f24118p168debjsnaf001c13d3f5',    
+    XRapidAPIKeyHeaderValue:'cb1c70ae9emsh3f5025072f24118p168debjsnaf001c13d3f5'
+    ,    
 };
